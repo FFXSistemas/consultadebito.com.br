f60e31ac53be8fb4e016af2ebb4e8612ae333b9f
Leandro souza <leandro.dasilvati7@gmail.com>
2018-08-13 18:07:49 -0300

    developing site consulta debito

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,12 @@ let cleanCSS = require('gulp-clean-css');
 
 let destOrigin = 'resources/assets/template/sass/**/*.sass';
 let destOut = 'resources/assets/template/css';
+let destOutWatch = 'resources/assets/template/css/*.css';
 
 
 
 const  buildCss = [
-    'resources/assets/template/css/geral.css',
+    "resources/assets/template/css/*.css",
 ]
 
 const webAssetsDir = 'public/build/'
@@ -22,17 +23,20 @@ const webAssetsDir = 'public/build/'
 
     gulp.task('sass:watch', function () {
         gulp.watch(destOrigin,['sass']);
-    });
+    })
 
-    gulp.task('watch', function () {
-        gulp.watch(destOrigin,['sass']);
-    });
-
-    gulp.task('workInCss', function () {
-        gulp.src(buildCss)
+    gulp.task('workInCss',function () {
+        return gulp.src(buildCss)
             .pipe(concat('template.css'))
-            .pipe(cleanCSS({debug: true}))
             .pipe(gulp.dest(webAssetsDir+'css/'))
     })
 
-    gulp.task('default',['sass','watch','workInCss']);
+    gulp.task('watch', function () {
+        // Uma lista de observação em tempo real, definindo que tarefa será executando em determinados arquivos ou pastas
+        gulp.watch(destOrigin,['sass']);
+        gulp.watch(destOutWatch,['workInCss']);
+    });
+
+
+    gulp.task('default',['sass','watch','workInCss']
+    );
